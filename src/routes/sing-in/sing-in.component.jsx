02373e89e60b9ec6c0e2dea6a014eeb4dd45279b
YonatanSchuster <yonatanmsch@gmail.com>
2022-11-09 17:15:42 +0200

    added sing-in componnent as well as firebase utils and brought in firbase library, also creating Document from google auth

--- src/routes/sing-in/sing-in.component.jsx
+++ src/routes/sing-in/sing-in.component.jsx
@@ -1,16 +1,16 @@
-import { signInWithGooglePopup } from "../../utils/firebase/firebase.utils";
+import { createUserDocumentFromAuth, signInWithGooglePopup } from "../../utils/firebase/firebase.utils";
 
 const SingIn = () => {
-  const logGoogleUser = async () => {
-    const response = await signInWithGooglePopup();
-    console.log(response);
+    const logGoogleUser = async () => {
+      const { user } = await signInWithGooglePopup();
+      const userDocRef = await createUserDocumentFromAuth(user);
+    };
+  
+    return (
+      <div>
+        <h1>Sign In Page</h1>
+        <button onClick={logGoogleUser}>Sign in with Google Popup</button>
+      </div>
+    );
   };
-
-  return (
-    <div>
-      <h1> Sing In Page</h1>
-      <button onClick={logGoogleUser}>Sing in with Google popup</button>
-    </div>
-  );
-};
 export default SingIn;
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -5,6 +5,7 @@ import {
   signInWithPopup,
   GoogleAuthProvider,
 } from "firebase/auth";
+import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBx5ypHDr34c8O9-QZ_SvfAFs7b3LSa1jI",
@@ -20,8 +21,39 @@ const firebaseApp = initializeApp(firebaseConfig);
 
 const provider = new GoogleAuthProvider();
 provider.setCustomParameters({
-  prompt: "select_account", //when sombody interacts with our provider, we allways want to force them to select account.
+  prompt: "select_account", // when sombody interacts with our provider, we allways want to force them to select account.
 });
 
 export const auth = getAuth();
 export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+  const userDocRef = doc(db, "userd", userAuth.uid);
+
+  console.log(userDocRef);
+
+  const userSnapshot = await getDoc(userDocRef); // allows us to check whether or not there is an instance of it that exists inside of our database, And it also allows us to access the data.
+  console.log(userSnapshot);
+  console.log(userSnapshot.exists());
+
+  //if user data !exist -> create/set the document with data from userAuth in my collection
+  if (!userSnapshot.exists()) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createdAt,
+      });
+    } catch (error) {
+      console.log('error creating the user', error.message);
+    }
+  }
+
+  //if user data exist -> return userDocRef
+  return userDocRef;
+};
