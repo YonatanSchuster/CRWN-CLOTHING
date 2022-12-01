5ca7246020283712179b4f829d871c66c17dfffe
YonatanSchuster <yonatanmsch@gmail.com>
2022-12-01 11:56:12 +0200

    creating cart reducer

---
+++ src/utils/reducer/reducer.utils.js
@@ -0,0 +1,2 @@
+// helper function - recives a type and payload, return back an obj so we dont need to type oout the namr of the keys
+export const createAction = (type, payload) => ({ type, payload });
